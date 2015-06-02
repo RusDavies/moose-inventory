21393320e06b0e8a74686012f416fe9dd2ffc777
russell <russell.davies@blakemere.ca>
2015-06-02 13:20:37 -0400

    About to try an rubocop autofix on models.rb

--- lib/moose/inventory/db/models.rb
+++ lib/moose/inventory/db/models.rb
@@ -1,28 +1,24 @@
-module Moose  
+module Moose
   module Inventory
     module DB
 
       class Host < Sequel::Model
         many_to_many :groups
-        one_to_many  :hostvars
-        #self.raise_on_save_failure = true
+        one_to_many :hostvars
       end
-      
+
       # TODO: Groups of groups? (i.e. a group with children?)
       class Group < Sequel::Model
         many_to_many :hosts
-        one_to_many  :groupvars
-        #self.raise_on_save_failure = true
+        one_to_many :groupvars
       end
-      
+
       class Hostvar < Sequel::Model
         many_to_one :hosts
-        #self.raise_on_save_failure = true
       end
-      
+
       class Groupvar < Sequel::Model
         many_to_one :groups
-        #self.raise_on_save_failure = true
       end
     end
   end
