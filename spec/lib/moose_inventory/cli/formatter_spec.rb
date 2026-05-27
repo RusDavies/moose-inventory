3f00ef18f554b04d119753c8fff2dfea81eacc44
Skippy <russell@blakemere.ca>
2026-05-27 12:01:32 -0400

    Add formatter branch coverage

--- spec/lib/moose_inventory/cli/formatter_spec.rb
+++ spec/lib/moose_inventory/cli/formatter_spec.rb
@@ -71,4 +71,86 @@ RSpec.describe Moose::Inventory::Cli::Formatter do
       expected(actual, desired)
     end
   end
+
+  describe 'stream helpers' do
+    it 'puts() writes indented text to stdout by default' do
+      actual = runner { @formatter.puts(2, 'hello world') }
+
+      desired = { aborted: false, STDOUT: "  hello world\n", STDERR: '' }
+
+      expected(actual, desired)
+    end
+
+    it 'puts() writes indented text to stderr when requested' do
+      actual = runner { @formatter.puts(2, 'hello world', 'STDERR') }
+
+      desired = { aborted: false, STDOUT: '', STDERR: "  hello world\n" }
+
+      expected(actual, desired)
+    end
+
+    it 'puts() aborts on an unknown output stream' do
+      actual = runner { @formatter.puts(2, 'hello world', 'BOGUS') }
+
+      desired = { aborted: true, STDOUT: '', STDERR: "Output stream 'BOGUS' is not known.\n" }
+
+      expected(actual, desired)
+    end
+
+    it 'print() writes indented text to stdout by default' do
+      actual = runner { @formatter.print(2, 'hello world') }
+
+      desired = { aborted: false, STDOUT: '  hello world', STDERR: '' }
+
+      expected(actual, desired)
+    end
+
+    it 'print() writes indented text to stderr when requested' do
+      actual = runner { @formatter.print(2, 'hello world', 'STDERR') }
+
+      desired = { aborted: false, STDOUT: '', STDERR: '  hello world' }
+
+      expected(actual, desired)
+    end
+
+    it 'print() aborts on an unknown output stream' do
+      actual = runner { @formatter.print(2, 'hello world', 'BOGUS') }
+
+      desired = { aborted: true, STDOUT: '', STDERR: "Output stream 'BOGUS' is not known.\n" }
+
+      expected(actual, desired)
+    end
+
+    it 'info() writes to stderr when requested' do
+      actual = runner { @formatter.info(2, 'hello world', 'STDERR') }
+
+      desired = { aborted: false, STDOUT: '', STDERR: '  INFO: hello world' }
+
+      expected(actual, desired)
+    end
+
+    it 'info() aborts on an unknown output stream' do
+      actual = runner { @formatter.info(2, 'hello world', 'BOGUS') }
+
+      desired = { aborted: true, STDOUT: '', STDERR: "Output stream 'BOGUS' is not known.\n" }
+
+      expected(actual, desired)
+    end
+
+    it 'warn() writes to stderr with the warning prefix' do
+      actual = runner { @formatter.warn('hello world') }
+
+      desired = { aborted: false, STDOUT: '', STDERR: 'WARNING: hello world' }
+
+      expected(actual, desired)
+    end
+
+    it 'error() writes to stderr with the error prefix' do
+      actual = runner { @formatter.error('hello world') }
+
+      desired = { aborted: false, STDOUT: '', STDERR: 'ERROR: hello world' }
+
+      expected(actual, desired)
+    end
+  end
 end
